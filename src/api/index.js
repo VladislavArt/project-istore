5017e319e07be648c6413bbb589413f37aa444a7
Vladislav Milrat <vrooog@gmail.com>
2025-05-21 14:22:53 +0300

    update package and deploy

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,8 @@
+import { serverPath } from "../helpers/variables"
+
 export const fetchProducts = async (params = '') => {
   try {
-    const response = await fetch('http://localhost:4000/products' + params)
+    const response = await fetch(serverPath + 'products/' + params)
     if(!response.ok) throw new Error('Ошибка получения продуктов')
     return response.json()
   } catch(error) {
@@ -10,7 +12,7 @@ export const fetchProducts = async (params = '') => {
 
 export const fetchCats = async () => {
   try {
-    const response = await fetch('http://localhost:4000/categories')
+    const response = await fetch(serverPath + 'categories')
     if(!response.ok) throw new Error('Ошибка получения категорий')
     return response.json()
   } catch (error) {
@@ -21,7 +23,7 @@ export const fetchCats = async () => {
 // Filter
 export const fetchFilterValues = async () => {
   try {
-    const response = await fetch('http://localhost:4000/filterValues')
+    const response = await fetch(serverPath + 'filterValues')
     if(!response.ok) throw new Error('Ошибка получения значений фильтра')
     return response.json()
   } catch(error) {
@@ -31,7 +33,7 @@ export const fetchFilterValues = async () => {
 
 export const fetchProduct = async (id = '') => {
   try {
-    const response = await fetch('http://localhost:4000/products/' + id)
+    const response = await fetch(serverPath + 'products/' + id)
     if(!response.ok) throw new Error('Ошибка получения продукта')
     return response.json()
   } catch(error) {
@@ -41,7 +43,7 @@ export const fetchProduct = async (id = '') => {
 
 export const createOrder = async (order)=> {
   try {
-    const response = await fetch('http://localhost:4000/orders/', {
+    const response = await fetch(serverPath + 'orders/', {
       method: 'POST',
       headers: {
         'Content-type': 'application/json'
